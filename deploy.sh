1142747d29993c93a7723520b7bd5fcbdb4fc9af
Bert Spaan <bert.spaan@gmail.com>
2018-11-28 23:09:00 +0100

    added deploy script

---
+++ deploy.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+npm run build
+
+# navigate into the build output directory
+cd dist
+
+cp ../CNAME ./
+
+git init
+git add -A
+git commit -m 'deploy'
+
+
+git push -f https://github.com/mmmenno/alida.git master:gh-pages
+
+cd -
